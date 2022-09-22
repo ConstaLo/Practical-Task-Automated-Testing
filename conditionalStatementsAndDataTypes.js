933b2a1789438954f5580a91490137bdadb20caa
Konstantin Loshchinin <llaconst2@gmail.com>
2022-09-22 17:41:04 +0500

    Completed part of the task - addition

---
+++ conditionalStatementsAndDataTypes.js
@@ -0,0 +1,7 @@
+// Perform addition of various types (string + boolean, string + number, number + boolean)
+let someString = 'Hello World';
+let someBoolean = true;
+let someNumber = 4;
+console.log('Result of str + bool = ', someString + someBoolean);
+console.log('Result of str + num = ', someString + someNumber);
+console.log('Result of num + bool = ', someNumber + someBoolean);
