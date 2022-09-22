c8a4021626a081076134cdbf4059ed359c4cfc2a
Konstantin Loshchinin <llaconst2@gmail.com>
2022-09-22 19:40:17 +0500

    Complete the task

--- conditionalStatementsAndDataTypes.js
+++ conditionalStatementsAndDataTypes.js
@@ -15,3 +15,11 @@ console.log('Result of num * bool = ', someNumber * someBoolean);
 console.log('Result of str / bool = ', someString / someBoolean);
 console.log('Result of str / num = ', someString / someNumber);
 console.log('Result of num / bool = ', someNumber / someBoolean);
+
+// Perform explicit conversion (number, string, boolean)
+let typeConversion = [123, '123', 'Hello', '', ' 123 ', 'He11o', ' ', true, false, NaN, null, undefined];
+for (i = 0; i <= typeConversion.length; i++) {
+    console.log(Boolean(typeConversion[i]), ' - result in ' + typeof !!typeConversion[i]);
+    console.log(Number(typeConversion[i]), ' - result in ' + typeof +typeConversion[i]);
+    console.log(String(typeConversion[i]), ' - result in ' + typeof `${typeConversion[i]}`);
+}
