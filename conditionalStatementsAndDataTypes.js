2dca504b3464a106432fdaf4ee5a43808d207f7d
Konstantin Loshchinin <llaconst2@gmail.com>
2022-09-22 17:57:57 +0500

    Completed part of the task - multiplication

--- conditionalStatementsAndDataTypes.js
+++ conditionalStatementsAndDataTypes.js
@@ -5,3 +5,8 @@ let someNumber = 4;
 console.log('Result of str + bool = ', someString + someBoolean);
 console.log('Result of str + num = ', someString + someNumber);
 console.log('Result of num + bool = ', someNumber + someBoolean);
+
+// Perform multiplication of various types (string * boolean, string * number, number * boolean)
+console.log('Result of str * bool = ', someString * someBoolean);
+console.log('Result of str * num = ', someString * someNumber);
+console.log('Result of num * bool = ', someNumber * someBoolean);
